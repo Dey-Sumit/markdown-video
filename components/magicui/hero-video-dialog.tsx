11c6ec87db9535cb8bd1d9d2d582a1e1f7bf2bac
Sumit Dey <dev.sumitdey@gmail.com>
2024-12-27 17:35:05 +0530

    landing page v2

--- components/magicui/hero-video-dialog.tsx
+++ components/magicui/hero-video-dialog.tsx
@@ -89,21 +89,21 @@ export default function HeroVideoDialog({
         className="group relative h-full cursor-pointer"
         onClick={() => setIsVideoOpen(true)}
       >
-        {/* <img
+        <img
           src={thumbnailSrc}
           alt={thumbnailAlt}
           // width={1920}
           // height={1080}
           className="max-h-full w-full rounded-md object-cover shadow-lg transition-all duration-200 ease-out group-hover:brightness-[0.8]"
-        /> */}
-        <video
+        />
+        {/* <video
           src="/landing-page/compare-video.mp4"
           autoPlay
           muted
           loop
           className="max-h-full"
-        />
-        <div className="absolute inset-0 flex items-center justify-center rounded-2xl bg-black/30">
+        /> */}
+        <div className="absolute inset-0 flex items-center justify-center rounded-2xl">
           <div className="flex size-28 scale-[0.9] items-center justify-center rounded-full bg-primary/10 backdrop-blur-md transition-all duration-200 ease-out group-hover:scale-100">
             <div
               className={`relative flex size-20 scale-100 items-center justify-center rounded-full bg-gradient-to-b from-primary/30 to-primary shadow-md transition-all duration-200 ease-out group-hover:scale-[1.2]`}
