1f5e6d268fd2d42fc4928886f7ae2fd58a5aa0e0
Sumit Dey <dev.sumitdey@gmail.com>
2024-12-28 20:57:17 +0530

    added a few improvements

--- app/page.tsx
+++ app/page.tsx
@@ -27,18 +27,25 @@ const LandingPage = () => {
         </div>
 
         <div className="mb-16 mt-20 flex flex-col items-center justify-center gap-y-6 text-center md:mb-20 md:mt-36">
-          <h1 className="text-4xl font-bold md:text-5xl lg:text-6xl xl:text-[5rem]">
-            Markdown to Video. That's it
+          <h1 className="relative text-4xl font-bold text-white md:text-5xl lg:text-6xl xl:text-[7rem]">
+            Markdown to Video
+            <div className="absolute -bottom-8 -right-20 flex -rotate-12 items-center justify-center gap-2 rounded-full border bg-gray-100/20 px-4 py-2 font-sans text-base font-normal tracking-wide">
+              <p className="inline-block bg-white bg-clip-text text-transparent">
+                as simple as that
+              </p>
+              <span>🤌</span>
+            </div>
           </h1>
 
-          <p className="mx-auto max-w-xl text-sm font-light text-gray-300 sm:max-w-2xl md:text-xl">
-            Make code walkthrough videos just by writing a simple markdown. As
-            simple as that. No need to learn complex video editing tools.
+          <p className="mx-auto max-w-xl text-sm text-gray-300 sm:max-w-2xl md:text-base">
+            Make code walkthrough videos just by writing a simple markdown. It's
+            magic (not really, just some coding you know) . No need to learn
+            complex video editing tools.
           </p>
 
           <div className="relative w-full sm:w-auto">
             <a href="#waitlist">
-              <AnimatedGradientText className="flex items-center justify-center rounded-full bg-white px-4 py-2 sm:py-3">
+              <AnimatedGradientText className="flex items-center justify-center rounded-full bg-white px-3 py-2 sm:py-3 md:px-4">
                 🧑‍🍳 <hr className="mx-2 h-4 w-[1px] shrink-0 bg-gray-300" />{" "}
                 <span
                   className={cn(
@@ -63,26 +70,6 @@ const LandingPage = () => {
           <SparklesText text="Markdown Video" />
         </div>
       </div>
-
-      {/* <div className="sticky bottom-0 left-0 z-0 flex h-80 w-full items-center justify-center bg-white">
-        <div className="relative flex h-full w-full items-start justify-end overflow-hidden px-12 py-12 text-right text-[#ff5941]">
-          <div className="sm:pace-x-16 flex flex-row space-x-12 text-sm sm:text-lg md:space-x-24 md:text-xl">
-            <ul>
-              <li className="cursor-pointer hover:underline">Home</li>
-              <li className="cursor-pointer hover:underline">Docs</li>
-              <li className="cursor-pointer hover:underline">Comps</li>
-            </ul>
-            <ul>
-              <li className="cursor-pointer hover:underline">Github</li>
-              <li className="cursor-pointer hover:underline">Instagram</li>
-              <li className="cursor-pointer hover:underline">X (Twitter)</li>
-            </ul>
-          </div>
-          <h2 className="font-calendas absolute bottom-0 left-0 translate-y-1/3 text-[80px] text-[#ff5941] sm:text-[192px]">
-            fancy
-          </h2>
-        </div>
-      </div> */}
     </div>
   );
 };
