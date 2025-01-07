565c4f35dcb6a71bacb5b6a246ca85fa7f897510
Sumit Dey <dev.sumitdey@gmail.com>
2025-01-07 21:24:30 +0530

    added decorator

--- components/x-editor/plugins/scene/scene.config.ts
+++ components/x-editor/plugins/scene/scene.config.ts
@@ -2,6 +2,12 @@
 
 import type { AdapterConfig } from "../../core/types/adapter.type";
 
+export const defaultSceneArgValues = {
+  duration: 5,
+  background: "transparent",
+  title: "scene-title",
+};
+
 export const sceneConfig: AdapterConfig = {
   id: "scene",
   pattern: {
@@ -16,8 +22,8 @@ export const sceneConfig: AdapterConfig = {
       type: "number",
       description: "Scene duration in seconds",
       required: true,
-      default: 5,
-      min: 0,
+      default: defaultSceneArgValues.duration,
+      min: 3,
       examples: {
         "3": "Short scene",
         "5": "Standard scene",
@@ -31,8 +37,8 @@ export const sceneConfig: AdapterConfig = {
         },
         {
           type: "range",
-          message: "Duration must be between 0 and 60 seconds",
-          validate: (value) => Number(value) >= 0 && Number(value) <= 60,
+          message: "Duration must be between 3 and 60 seconds",
+          validate: (value) => Number(value) >= 3 && Number(value) <= 60,
           severity: "error",
         },
       ],
@@ -42,33 +48,31 @@ export const sceneConfig: AdapterConfig = {
       type: "string",
       description: "Background color",
       examples: {
-        transparent: "Default transparent background",
-        black: "Solid black background",
-        white: "Solid white background",
+        transparent: "Default transparent ",
+        white: "White background",
+        black: "Black background",
+        red: "Red background",
+        [`"linear-gradient(40deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%)"`]:
+          "Linear gradient ",
+        [`"radial-gradient(circle, #237A57 25%, #093028 100%)"`]:
+          "Radial gradient ",
+        [`https://images.pexels.com/photos/3075993/pexels-photo-3075993.jpeg`]:
+          "Colorful abstract background",
       },
       validations: [
         {
           type: "enum",
           message: "Invalid background value",
-          validate: (value) =>
-            ["transparent", "black", "white"].includes(value),
-          severity: "warning",
         },
       ],
-      default: "transparent",
+      default: defaultSceneArgValues.background,
     },
     title: {
       name: "title",
       type: "string",
       description: "Scene identifier",
       required: false,
-      default: "scene-title",
+      default: defaultSceneArgValues.title,
     },
   },
-};
-
-export const defaultSceneArgValues = {
-  duration: 5,
-  background: "transparent",
-  title: "scene-title",
 };
